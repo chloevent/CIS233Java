44de6418f298c7bb52619f88c341bb598b55ef92
Chloe Viriyapunt <chloe.viriyapunt@gmail.com>
2017-10-20 16:07:43 -0700

    Print Date

--- src/CIS233J/HelloWorld.java
+++ src/CIS233J/HelloWorld.java
@@ -1,12 +1,16 @@
 package CIS233J;
+
 /*
  * Author: Chloe Viriyapunt
  * Version: 10/20/2014
  */
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 
 }
