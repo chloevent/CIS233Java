52dc0bd2fb929de54bf970017b42c8d49154de8a
Chloe Viriyapunt <chloe.viriyapunt@gmail.com>
2017-10-20 15:58:44 -0700

    Added main method that prints hello world

--- src/CIS233J/HelloWorld.java
+++ src/CIS233J/HelloWorld.java
@@ -1,8 +1,12 @@
 package CIS233J;
 /*
- * Name: Chloe Viriyapunt
+ * Author: Chloe Viriyapunt
+ * Version: 10/20/2014
  */
 
 public class HelloWorld {
-    
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+
 }
